b782077f3a15bd4b6e4d62f6f5512843d51294fd
Arbain02 <147217625+Arbain02@users.noreply.github.com>
2023-10-20 21:15:33 +0700

    Add files via upload

---
+++ Arbain_day12_percabngan.java
@@ -0,0 +1,15 @@
+
+package arbain_day12_percabngan;
+
+public class Arbain_day12_percabngan {
+
+  
+    public static void main(String[] args) {
+        if (12 > 100)
+            System.out.println("Tetap semangat");
+        else {
+            System.out.println("Lulus");
+        }
+    }
+    
+}
