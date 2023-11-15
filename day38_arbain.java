63e92195e6a90b1ab9f71e1e2e9bad36c0e9571f
Arbain02 <147217625+Arbain02@users.noreply.github.com>
2023-11-15 22:57:43 +0800

    Add files via upload

---
+++ day38_arbain.java
@@ -0,0 +1,28 @@
+
+package isc;
+
+import java.util.Scanner;
+
+public class day38_arbain {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Masukkan panjang alas jajargenjang: ");
+        double alas = scanner.nextDouble();
+
+        System.out.println("Masukkan tinggi jajargenjang: ");
+        double tinggi = scanner.nextDouble();
+
+        // Hitung luas jajargenjang
+        double luas = alas * tinggi;
+
+        // Hitung keliling jajargenjang
+        double keliling = 2 * (alas + tinggi);
+
+        System.out.println("Luas jajargenjang: " + luas);
+        System.out.println("Keliling jajargenjang: " + keliling);
+
+        scanner.close();
+    }
+  
+}
